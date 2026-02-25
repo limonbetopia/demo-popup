5f085e22da8e26eaf706e237e782c34b388e6389
limonbetopia <limonbdcalling@gmail.com>
2026-02-25 16:15:20 +0600

    popup

--- app/routes/app.Popups.jsx
+++ app/routes/app.Popups.jsx
@@ -1,3 +1,5 @@
+import frame70 from "../images/Frame70.png";
+
 function Popups() {
   return (
     <>
@@ -5,6 +7,7 @@ function Popups() {
         <Top></Top>
         <PopupAraEmty></PopupAraEmty>
         <Modal></Modal>
+        <Filter></Filter>
       </s-page>
     </>
   );
@@ -68,27 +71,82 @@ function PopupAraEmty() {
 
 function Modal() {
   return (
-    <s-banner
-      heading="127 products missing shipping weights"
-      tone="warning"
-      dismissible
-    >
-      Products without weights may show inaccurate shipping rates, leading to
-      checkout abandonment.
-      <s-button
-        slot="secondary-actions"
-        variant="secondary"
-        href="/admin/products?filter=missing-weights"
-      >
-        Review products
-      </s-button>
-      <s-button
-        slot="secondary-actions"
-        variant="secondary"
-        href="javascript:void(0)"
-      >
-        Setup guide
+    <s-banner heading="App embed is not enabled" tone="warning" dismissible>
+      <s-box inlineSize="400px">
+        To display your published popups, the app embed needs to be enabled.
+        Please enable it to show popups on your storefront.
+      </s-box>
+
+      <s-button slot="secondary-actions" variant="secondary">
+        Enable now
       </s-button>
     </s-banner>
+  );
+}
+
+// for popup filter
+
+function Filter() {
+  return (
+    <>
+      <s-stack>
+        <s-stack direction="inline" justifyContent="space-between">
+          <>
+            <s-button
+              commandFor="customer-menu"
+              variant="primary"
+              alignItems="center"
+            >
+              <s-icon type="note"></s-icon>
+              All popups
+            </s-button>
+
+            <s-menu id="customer-menu" accessibilityLabel="Customer actions">
+              <s-button tone="neutral">Active</s-button>
+              <s-button tone="critical">Draft</s-button>
+            </s-menu>
+          </>
+
+          <>
+            <s-button commandFor="customer-menu2" variant="primary">
+              <s-icon type="sort"></s-icon>
+              Sort by: Last edited
+            </s-button>
+
+            <s-menu id="customer-menu2" accessibilityLabel="Customer actions">
+              <s-button>Last edited</s-button>
+              <s-button>Created</s-button>
+              <s-button>Updated</s-button>
+            </s-menu>
+          </>
+        </s-stack>
+      </s-stack>
+      <br />
+      <s-section padding="none">
+        <s-table>
+          <s-table-header-row>
+            <s-table-header>Popup name</s-table-header>
+            <s-table-header>Active</s-table-header>
+            <s-table-header>Views</s-table-header>
+            <s-table-header>Subscriber</s-table-header>
+            <s-table-header>Conversion</s-table-header>
+            <s-table-header></s-table-header>
+          </s-table-header-row>
+
+          <s-table-body>
+            <s-table-row>
+              <s-table-cell>
+                <s-stack>
+                  <s-image src={frame70} alt={frame70} />
+                </s-stack>
+              </s-table-cell>
+              <s-table-cell>john@example.com</s-table-cell>
+              <s-table-cell>23</s-table-cell>
+              <s-table-cell>123-456-7890</s-table-cell>
+            </s-table-row>
+          </s-table-body>
+        </s-table>
+      </s-section>
+    </>
   );
 }
